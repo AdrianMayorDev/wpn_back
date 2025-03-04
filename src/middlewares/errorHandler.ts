d15c31e381c472401f26b0e59c110396ed49f42a
Adrian Mayor <104377546+AdrianMayor@users.noreply.github.com>
2025-03-04 15:57:38 +0100

    testing husky

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,11 @@
-import { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from 'express';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-	console.error(err.stack);
-	res.status(500).send({ error: "Something went wrong!" });
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+): void => {
+  console.error(err.stack);
+  res.status(500).send({ error: 'Something went wrong!' });
 };
