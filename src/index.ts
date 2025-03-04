c8f4f26306c741df40c55609c4f854e3fad2f092
Adrian Mayor <104377546+AdrianMayor@users.noreply.github.com>
2025-03-04 17:58:01 +0100

    cleaning index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const port = process.env.SERVER_PORT ?? 3005;
 
 app.use(express.json());
 app.use('/user', userRoutes);
-// test
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
